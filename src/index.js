fc396fbd9d65806295ebf5cad327959d378c54de
Joeyker <oswaldo_avilez@outlook.es>
2025-01-20 16:49:12 -0300

    ajustes

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import express from 'express';
-import {dirname,join} from 'path';
+import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
-import routes from '../src/routes/index.js';
-
+import routes from './routes/index.js';  
 const app = express();
 
-const __dirname = dirname(fileURLToPath(import.meta.url))
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.set('views',join(__dirname,'views'));
-app.set('view engine', 'ejs')
+app.set('views', join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.use(routes);
 
-app.use(express.static(join(__dirname,'public')));
+app.use(express.static(join(__dirname, 'public')));
 
+const PORT = process.env.PORT || 3000;  
 
-
-
-
-app.listen(3000)
-console.log("server is listening on port", 3000)
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
